f793f71fd8458d22e9fc56aa5f604d0d6c06d791
Ludds <Ludvig.Andersson@solidbeans.com>
2023-01-21 17:42:48 +0100

    Add a footer
    Add a simple footer for the website with contact info

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react"
+
+function Footer() {
+
+    return (
+        <>
+            <div css={css`height: 150px; margin-top: 100px; padding-top: 50px; border-top: 1px solid lightgrey; text-align: center;`}>
+                <p>
+                    Consultant <a href="https://www.solidbeans.com/" css={css`text-decoration: none; color: #FF761F; &:hover {color: #FF761F; text-decoration:none; cursor:pointer; font-weight: 600;}`}>
+                        @ Solid Beans
+                    </a> | <a href="https://www.linkedin.com/in/ludvig-andersson-la/" css={css`text-decoration: none; color: #050504; &:hover {color: #050504; text-decoration:none; cursor:pointer; font-weight: 600;}`}>
+                        LinkedIn
+                    </a>
+                </p>
+            </div>
+        </>
+    );
+}
+
+export default Footer;
