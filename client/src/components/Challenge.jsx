fb459fce2b71e4e2191f4117aae82223d6c27154
Ludds <Ludvig.Andersson@solidbeans.com>
2023-06-20 14:02:17 +0200

    Remove unused import

--- client/src/components/Challenge.jsx
+++ client/src/components/Challenge.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import ImageWithText from "./ImageWithText";
-/** @jsxImportSource @emotion/react */
-import { css } from "@emotion/react"
 
 function Challenge(props) {
     return (
