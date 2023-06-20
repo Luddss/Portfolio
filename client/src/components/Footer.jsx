cdd63b7baf45d68d3d88d4285d718e8076be6c9a
Ludds <Ludvig.Andersson@solidbeans.com>
2023-06-20 14:52:46 +0200

    Remove company association

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -8,20 +8,13 @@ function Footer() {
         <>
             <div css={css`height: 150px; margin-top: 100px; padding-top: 50px; border-top: 1px solid lightgrey;`}>
                 <div css={{ display: 'flex', justifyContent: 'center', gap: '2px' }}>
-                    <p>Consultant</p>
-                    <div css={{ width: '107px' }}>
-                        <p>
-                            <a href="https://www.solidbeans.com/" target="_blank" rel="noopener noreferrer" css={css`text-decoration: none; color: #FF761F;  transition: 0.1s; &:hover {color: #FF761F; text-decoration:none; cursor:pointer; font-weight: 600;}`}>
-                                @ Solid Beans
-                            </a>
-                        </p>
-                    </div>
+                    <p>Available for work</p>
                     <p>
                         |
                     </p>
                     <div css={{ width: '66px' }}>
                         <p>
-                            <a href="https://www.linkedin.com/in/ludvig-andersson-la/" target="_blank" rel="noopener noreferrer" css={css`text-decoration: none; color: #050504; transition: 0.1s; &:hover {color: #050504; text-decoration:none; cursor:pointer; font-weight: 600;}`}>
+                            <a href="https://www.linkedin.com/in/ludvig-andersson-la/" target="_blank" rel="noopener noreferrer" css={css`text-decoration: none; color: #FF761F; transition: 0.1s; &:hover {color: #FF761F; text-decoration:none; cursor:pointer; font-weight: 600;}`}>
                                 LinkedIn
                             </a>
                         </p>
